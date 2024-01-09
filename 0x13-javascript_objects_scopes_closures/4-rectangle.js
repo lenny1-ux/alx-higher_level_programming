c165d0ed42a329368d61437c7eb1d0af6f44863c
lenny1-ux <leonardsammy01@gmail.com>
2024-01-09 14:10:52 -0800

    debuging

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -16,3 +16,18 @@ class Rectangle {
       console.log(s);
     }
   }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
+
