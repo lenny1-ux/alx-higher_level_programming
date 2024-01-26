d2e6dfde6c6abe398eaee0e5b64d4587de66efb8
lenny1-ux <leonardsammy01@gmail.com>
2024-01-26 10:27:25 -0800

    debugging 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,6 @@
 """The script that requests content from `https://intranet.hbtn.io/status`"""
 
 import urllib.request
-import urllib.error
 
 try:
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
