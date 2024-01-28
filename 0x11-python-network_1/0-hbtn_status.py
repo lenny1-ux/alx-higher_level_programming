ab6b59d58a7a96794afd62fb1262bbd2a4b00d72
lenny1-ux <leonardsammy01@gmail.com>
2024-01-27 22:22:06 -0800

    fix all

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,10 +6,10 @@ The script that requests content from `https://intranet.hbtn.io/status`
 import urllib.request
 
 if __name__ == "__main__":
-    req = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(body)))
-        print('\t- content: {}'.format(body))
-        print('\t- utf8 content: {}'.format(body.decode('utf8')))
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,11 +2,18 @@
 """
 Post an email in the header and body response in utf-8
 """
-import urllib.request
-import sys
 
 if __name__ == "__main__":
-    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
-    req = urllib.request.Request(sys.argv[1])
-    with urllib.request.urlopen(req) as response:
-        print(response.read())
+   
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    argv = sys.argv
+    url = argv[1]
+    email = argv[2]
+    DATA = urllib.parse.urlencode({"email": email})
+    DATA = DATA.encode('ascii')
+
+    with urllib.request.urlopen(url, DATA) as response:
+        print(response.read().decode('utf-8'))
